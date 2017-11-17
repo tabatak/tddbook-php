02ed12ccf1cded8054991c24fe25e1b5ad2ea61b
tabatak <hamamoto.kazuto@gmail.com>
2017-11-17 20:35:58 +0900

    chapter03
    javaでいうところのObjectってどう書くんだろう。Dollarクラスを受け取ることにしちゃってて、ちょっとテキストとは異なる状態になっちゃってる。

---
+++ part1/chapter04/Dollar.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Money\chapter04;
+
+class Dollar
+{
+    private $amount;
+
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier): Dollar
+    {
+        return new Dollar( $this->amount * $multiplier);
+    }
+
+    public function equals(Dollar $dollar): bool
+    {
+        return $this->amount === $dollar->amount;
+    }
+}
