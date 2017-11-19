6520d99742cd3b8868baf655c226db4b264eeabf
tabatak <hamamoto.kazuto@gmail.com>
2017-11-19 19:09:50 +0900

    chapter09
    可視性の設定が適当な気がする

---
+++ part1/chapter09/Dollar.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Money\chapter09;
+
+class Dollar extends Money
+{
+    public function __construct(int $amount, string $currency)
+    {
+        parent::__construct($amount, $currency);
+    }
+
+    public function times(int $multiplier): Money
+    {
+        return Money::dollar($this->amount * $multiplier);
+    }
+}
